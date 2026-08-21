917ecea7d18f3de1e7e4570ea798daec763c6737
syedpycoder <92701599+syedpycoder@users.noreply.github.com>
2026-08-21 01:29:31 -0400

    Add: validate-parentheses - submission-17

---
+++ Data Structures & Algorithms/validate-parentheses/submission-17.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        char_dict = {'}':'{', ')':'(', ']':'['}
+        stack = []
+
+        for ch in s:
+            if ch not in char_dict.keys():
+                stack.append(ch)
+            else:
+                if stack and stack[-1] == char_dict[ch]:
+                    stack.pop()
+                else:
+                    return False
+        if not stack:
+            return True
+        return False                        
+
+       
